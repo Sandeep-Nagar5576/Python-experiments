a613d800815e389971d26f6baed8867e7006c308
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:01:54 +0530

    Update naturalNumbers.py

--- Sheet2/naturalNumbers.py
+++ Sheet2/naturalNumbers.py
@@ -1,5 +1,8 @@
-N = int(input("Enter a number: "))
-i = 1
-while i <= N:
-    print(i, end=" ")
-    i += 1
+''' A program that takes a positive integer N as input from the user and prints all natural numbers 
+numbers from 1 to N, with each number followed by a space.'''
+
+n=int(input("Enter a number"))
+i=0 
+while(i<=n):
+    print(i, end=' ')
+    i+=1
