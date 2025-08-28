ff2544e15dbdbeaf9e0f6c1acafc8ccd8f9dfc8b
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:01:18 +0530

    Update multiplicationTable.py

--- Sheet2/multiplicationTable.py
+++ Sheet2/multiplicationTable.py
@@ -1,5 +1,7 @@
-A = int(input("Enter a number: "))
-i = 1
-while i <= 10:
-    print(A, "*", i, "=", A * i)
-    i += 1
+''' Taking a number A as input, print its multiplication table having the first 10 multiples. '''
+
+n=int(input("Enter a number "))
+i=1
+while(i<=10):
+    print(n,"*",i,"=",n*i)
+    i+=1
