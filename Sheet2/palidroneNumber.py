18ed69bc7ab0b59178fd5b81861051341fd23e20
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:03:56 +0530

    Update palidroneNumber.py

--- Sheet2/palidroneNumber.py
+++ Sheet2/palidroneNumber.py
@@ -1,10 +1,17 @@
-A = int(input("Enter a number: "))
-rev = 0
-temp = A
-while temp > 0:
-    rev = rev * 10 + temp % 10
-    temp //= 10
-if rev == A:
-    print("Yes, Palindrome")
+''' You are given an integer A as input, and you need to determine whether it is a palindrome 
+or not. A palindrome integer is one whose digits, when reversed, result in the same number. 
+For example, 121 is a palindrome because its reverse is also 121, but 123 is not a palindrome 
+because its reverse is 321.Note: The given integer will not have any leading zeros. '''
+
+n=int(input("Enter a number "))
+original=n
+rev=0
+while(n>0):
+    r=n%10
+    rev=rev*10+r
+    n=n//10
+
+if(rev==original):
+    print("this  is a palindrone number")
 else:
-    print("No, Not Palindrome")
+    print("this  is not a palindrone number")
