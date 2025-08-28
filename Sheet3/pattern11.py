aca05f60545614e09139e77ba58a997dae51c8de
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 22:08:10 +0530

    Update pattern11.py

--- Sheet3/pattern11.py
+++ Sheet3/pattern11.py
@@ -1,3 +1,16 @@
+''' 
+********** 
+****  **** 
+***    *** 
+**      ** 
+*        * 
+*        * 
+**      ** 
+***    *** 
+****  **** 
+**********     '''   
+
+
 n=int(input("Enter the number:"))
 for i in range(1,n+1):
     for j in range(n-i+1):
@@ -15,4 +28,5 @@ for i in range(1,n+1):
         print(" ",end="")
     for j in range(i):
         print("*",end="")
+
     print()
