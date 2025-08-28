4722b87c04a7eff6bac540ccb08df4e834006009
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:05:41 +0530

    Update sumofevennum.py

--- Sheet2/sumofevennum.py
+++ Sheet2/sumofevennum.py
@@ -1,7 +1,18 @@
-A = int(input("Enter a number: "))
-i = 2
-total = 0
-while i <= A:
-    total += i
-    i += 2
-print("Sum of even numbers :", total)
+'''  You are given an integer A, you need to find and return the sum of all the even numbers 
+between 1 and A. Even numbers are those numbers that are divisible by 2. '''
+
+# n=int(input("Enter a number "))
+# sum=0
+# for i in range(0,n+1,2):
+#     sum+=i
+# print(sum,end=' ')
+
+
+n=int(input("Enter a number "))
+i=0
+sum=0
+while(i<=n):
+    if(i%2==0):
+     sum+=i
+    i+=1
+print(sum)
