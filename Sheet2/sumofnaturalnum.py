bdcbe8004e1e6e2b66e5327e86f1173333706dfb
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:06:22 +0530

    Update sumofnaturalnum.py

--- Sheet2/sumofnaturalnum.py
+++ Sheet2/sumofnaturalnum.py
@@ -1,7 +1,18 @@
-N = int(input("Enter a number: "))
-i = 1
-total = 0
-while i <= N:
-    total += i
-    i += 1
-print("Sum :", total)
+''' A program to find the sum of all Natural numbers from 1 to N, where you have to take N as 
+input from user '''
+
+n=int(input("Enter a number "))
+i=0
+sum=0
+while(i<=n):
+    sum=sum+i
+    i+=1
+
+print(sum)
+
+# n=int(input("Enter a number "))
+# sum=0
+# for i in range(0,n+1):
+#     sum+=i
+# print(sum)
+
