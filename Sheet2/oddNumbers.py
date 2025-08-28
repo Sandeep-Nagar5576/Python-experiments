14462d87a72aee0cfb5d99decb7dc6494a39623b
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:02:36 +0530

    Update oddNumbers.py

--- Sheet2/oddNumbers.py
+++ Sheet2/oddNumbers.py
@@ -1,5 +1,13 @@
-N = int(input("Enter a number: "))
-i = 1
-while i <= N:
-    print(i, end=" ")
-    i += 2
+''' A program to print all odd numbers from 1 to N, where you have to take N as input 
+     from user.'''
+#  n=int(input("Enter a number "))
+# for i in range(1,n+1,2):
+#     print(i,end=' ')
+
+
+n=int(input("Enter a number "))
+i=1
+while(i<=n):
+    if(i%2!=0):
+        print(i,end=' ')
+    i+=1
