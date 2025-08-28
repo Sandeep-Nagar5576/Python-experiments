477e7d48d857d16cf3eadef3e8b4ddbc88a83f72
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:04:19 +0530

    Update revNaturalNumbers.py

--- Sheet2/revNaturalNumbers.py
+++ Sheet2/revNaturalNumbers.py
@@ -1,5 +1,20 @@
-N = int(input("Enter a number: "))
-i = N
-while i >= 1:
-    print(i, end=" ")
-    i -= 1
+''' A program to print all Natural numbers from N to 1, where you have to take N as input 
+    from the user.'''
+    
+# n=int(input("Enter a number"))
+# for i in range(n,0,-1):
+#     print(i, end=' ')
+
+
+
+n=int(input("Enter a number "))
+i=n
+while(i<=n):
+    print(i, end=' ')
+    i-=1
+    if(i==0):
+        break
+
+
+
+
