6c9722402193a51e14dcd15420421afd3a35bbce
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 21:54:34 +0530

    Update pattern5.py

--- Sheet3/pattern5.py
+++ Sheet3/pattern5.py
@@ -1,3 +1,10 @@
+'''
+*_ _ _* 
+*_ _ _* 
+*_ _ _* 
+*_ _ _* 
+*_ _ _*  '''
+
 n=int(input("Enter the number:"))
 for i in range(n+1):
     for j in range(1,n+1):
@@ -5,4 +12,5 @@ for i in range(n+1):
             print("*",end=" ")
         else:
             print("_",end=" ")
+
     print()
