991907bfc66db3dadb4e28fe5f3953c7c368d64c
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 18:58:26 +0530

    Updating countOfDigits.py

--- Sheet2/countOfDigits.py
+++ Sheet2/countOfDigits.py
@@ -1,7 +1,13 @@
-N = int(input("Enter a number: "))
-count = 0
-temp = N
-while temp > 0:
-    count += 1
-    temp //= 10
-print("digits :", count)
+''' Taking an integer N as input and print the count of digits of that number.'''
+
+n=int(input("Enter a number "))
+count=0
+if n == 0:
+    count = 1
+else:
+    count = 0
+    while n > 0:
+        count += 1
+        n=n// 10
+
+print(count)
