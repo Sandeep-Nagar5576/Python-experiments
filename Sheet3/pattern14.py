84c094bdca205f940830dd9eda15cae6582fb9b1
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 22:10:23 +0530

    Update pattern14.py

--- Sheet3/pattern14.py
+++ Sheet3/pattern14.py
@@ -1,7 +1,14 @@
+'''
+1 
+2 3 
+4 5 6 
+7 8 9 10  '''
+
 n=int(input("Enter the number:"))
 num=1
 for i in range(1,n+1):
     for j in range(i):
         print(num,end=" ")
         num+=1
+
     print()
