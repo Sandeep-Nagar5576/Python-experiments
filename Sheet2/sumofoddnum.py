775cfe987754e5ab44f6ce28028ddedfba9ae395
Sandeep-Nagar5576 <sandeepnagar5576@gmail.com>
2025-08-28 19:07:29 +0530

    Update sumofoddnum.py

--- Sheet2/sumofoddnum.py
+++ Sheet2/sumofoddnum.py
@@ -1,7 +1,8 @@
-A = int(input("Enter a number: "))
-i = 1
-total = 0
-while i <= A:
-    total += i
-    i += 2
-print("Sum of odd numbers :", total)
+''' Taking an integer A as input. You have to print the sum of all odd numbers in the range [1,A].'''
+
+n=int(input("Enter a number "))
+sum=0
+for i in range(1,n,2):
+    sum+=i
+print(sum,end=' ')
+
